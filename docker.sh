2e342287360617f6aa59bcbf6f0776ce99a764d4
bbq923 <bbq9234@naver.com>
2017-07-20 17:07:56 +0900

    prepare to push docker image

--- docker.sh
+++ docker.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 ./gradlew buildDocker
-cd ~/docker/nginx
-docker build -t nginx:1.0.0 .
-docker stop bbqnginx
-docker stop bbqtrello
-docker rm bbqnginx
-docker rm bbqtrello
-docker run -d --name bbqtrello bbqtrello-docker
-docker run -d --name bbqnginx --link bbqtrello -p 80:80 nginx:1.0.0
+docker-compose down
+docker-compose up -d
+#cd ~/docker/nginx
+#docker build -t nginx:1.0.0 .
+#docker stop bbqnginx
+#docker stop bbqtrello
+#docker rm bbqnginx
+#docker rm bbqtrello
+#docker run -d --name bbqtrello bbqtrello-docker
+#docker run -d --name bbqnginx --link bbqtrello -p 80:80 nginx:1.0.0
 
 
